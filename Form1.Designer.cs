b8930d33f257ae74f9a876418b96f2cf8d7e7cdd
Muhammad Akib Anjum <akibanj2017@gmail.com>
2026-04-21 15:03:34 +0600

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace AutoCare_Pro
+{
+    partial class loginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginPage));
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.pnlRegister = new System.Windows.Forms.Panel();
+            this.pnlLoginForm = new System.Windows.Forms.Panel();
+            this.txtLoginPass = new System.Windows.Forms.TextBox();
+            this.lblLoginPass = new System.Windows.Forms.Label();
+            this.txtLoginEmail = new System.Windows.Forms.TextBox();
+            this.lblLoginEmail = new System.Windows.Forms.Label();
+            this.txtOutputPass = new System.Windows.Forms.TextBox();
+            this.lblRegPass = new System.Windows.Forms.Label();
+            this.txtRegEmail = new System.Windows.Forms.TextBox();
+            this.lblRegEmail = new System.Windows.Forms.Label();
+            this.txtRegUsername = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlCar = new System.Windows.Forms.Panel();
+            this.picCar = new System.Windows.Forms.PictureBox();
+            this.pnlLogin.SuspendLayout();
+            this.pnlRegister.SuspendLayout();
+            this.pnlLoginForm.SuspendLayout();
+            this.pnlCar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(74)))), ((int)(((byte)(102)))));
+            this.pnlLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLogin.Controls.Add(this.pnlLoginForm);
+            this.pnlLogin.Controls.Add(this.pnlRegister);
+            this.pnlLogin.Controls.Add(this.btnRegister);
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.lblDescription);
+            this.pnlLogin.Controls.Add(this.lblTitle);
+            this.pnlLogin.Controls.Add(this.pnlCar);
+            this.pnlLogin.Location = new System.Drawing.Point(329, 103);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(453, 388);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // pnlRegister
+            // 
+            this.pnlRegister.Controls.Add(this.txtOutputPass);
+            this.pnlRegister.Controls.Add(this.lblRegPass);
+            this.pnlRegister.Controls.Add(this.txtRegEmail);
+            this.pnlRegister.Controls.Add(this.lblRegEmail);
+            this.pnlRegister.Controls.Add(this.txtRegUsername);
+            this.pnlRegister.Controls.Add(this.lblUserName);
+            this.pnlRegister.Location = new System.Drawing.Point(65, 177);
+            this.pnlRegister.Name = "pnlRegister";
+            this.pnlRegister.Size = new System.Drawing.Size(324, 192);
+            this.pnlRegister.TabIndex = 5;
+            this.pnlRegister.Visible = false;
+            // 
+            // pnlLoginForm
+            // 
+            this.pnlLoginForm.Controls.Add(this.txtLoginPass);
+            this.pnlLoginForm.Controls.Add(this.lblLoginPass);
+            this.pnlLoginForm.Controls.Add(this.txtLoginEmail);
+            this.pnlLoginForm.Controls.Add(this.lblLoginEmail);
+            this.pnlLoginForm.Location = new System.Drawing.Point(68, 170);
+            this.pnlLoginForm.Name = "pnlLoginForm";
+            this.pnlLoginForm.Size = new System.Drawing.Size(325, 132);
+            this.pnlLoginForm.TabIndex = 6;
+            // 
+            // txtLoginPass
+            // 
+            this.txtLoginPass.Location = new System.Drawing.Point(15, 92);
+            this.txtLoginPass.Name = "txtLoginPass";
+            this.txtLoginPass.PasswordChar = '*';
+            this.txtLoginPass.Size = new System.Drawing.Size(289, 20);
+            this.txtLoginPass.TabIndex = 5;
+            // 
+            // lblLoginPass
+            // 
+            this.lblLoginPass.AutoSize = true;
+            this.lblLoginPass.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginPass.ForeColor = System.Drawing.Color.White;
+            this.lblLoginPass.Location = new System.Drawing.Point(12, 71);
+            this.lblLoginPass.Name = "lblLoginPass";
+            this.lblLoginPass.Size = new System.Drawing.Size(85, 18);
+            this.lblLoginPass.TabIndex = 4;
+            this.lblLoginPass.Text = "Password";
+            // 
+            // txtLoginEmail
+            // 
+            this.txtLoginEmail.Location = new System.Drawing.Point(15, 34);
+            this.txtLoginEmail.Name = "txtLoginEmail";
+            this.txtLoginEmail.Size = new System.Drawing.Size(289, 20);
+            this.txtLoginEmail.TabIndex = 3;
+            // 
+            // lblLoginEmail
+            // 
+            this.lblLoginEmail.AutoSize = true;
+            this.lblLoginEmail.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginEmail.ForeColor = System.Drawing.Color.White;
+            this.lblLoginEmail.Location = new System.Drawing.Point(12, 13);
+            this.lblLoginEmail.Name = "lblLoginEmail";
+            this.lblLoginEmail.Size = new System.Drawing.Size(53, 18);
+            this.lblLoginEmail.TabIndex = 2;
+            this.lblLoginEmail.Text = "Email";
+            // 
+            // txtOutputPass
+            // 
+            this.txtOutputPass.Location = new System.Drawing.Point(18, 149);
+            this.txtOutputPass.Name = "txtOutputPass";
+            this.txtOutputPass.PasswordChar = '*';
+            this.txtOutputPass.Size = new System.Drawing.Size(289, 20);
+            this.txtOutputPass.TabIndex = 5;
+            // 
+            // lblRegPass
+            // 
+            this.lblRegPass.AutoSize = true;
+            this.lblRegPass.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegPass.ForeColor = System.Drawing.Color.White;
+            this.lblRegPass.Location = new System.Drawing.Point(15, 128);
+            this.lblRegPass.Name = "lblRegPass";
+            this.lblRegPass.Size = new System.Drawing.Size(85, 18);
+            this.lblRegPass.TabIndex = 4;
+            this.lblRegPass.Text = "Password";
+            // 
+            // txtRegEmail
+            // 
+            this.txtRegEmail.Location = new System.Drawing.Point(18, 91);
+            this.txtRegEmail.Name = "txtRegEmail";
+            this.txtRegEmail.Size = new System.Drawing.Size(289, 20);
+            this.txtRegEmail.TabIndex = 3;
+            // 
+            // lblRegEmail
+            // 
+            this.lblRegEmail.AutoSize = true;
+            this.lblRegEmail.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegEmail.ForeColor = System.Drawing.Color.White;
+            this.lblRegEmail.Location = new System.Drawing.Point(15, 70);
+            this.lblRegEmail.Name = "lblRegEmail";
+            this.lblRegEmail.Size = new System.Drawing.Size(53, 18);
+            this.lblRegEmail.TabIndex = 2;
+            this.lblRegEmail.Text = "Email";
+            // 
+            // txtRegUsername
+            // 
+            this.txtRegUsername.Location = new System.Drawing.Point(18, 37);
+            this.txtRegUsername.Name = "txtRegUsername";
+            this.txtRegUsername.Size = new System.Drawing.Size(289, 20);
+            this.txtRegUsername.TabIndex = 1;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.ForeColor = System.Drawing.Color.White;
+            this.lblUserName.Location = new System.Drawing.Point(15, 16);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(90, 18);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "Username";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(46)))));
+            this.btnRegister.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(46)))));
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Location = new System.Drawing.Point(228, 126);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(116, 35);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(115)))), ((int)(((byte)(10)))));
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(46)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(118, 126);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(113, 35);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.ForeColor = System.Drawing.Color.Gray;
+            this.lblDescription.Location = new System.Drawing.Point(115, 97);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(229, 16);
+            this.lblDescription.TabIndex = 2;
+            this.lblDescription.Text = "Smart Billing For Mordern Garages";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(131, 68);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(202, 29);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "AUTOCARE PRO";
+            // 
+            // pnlCar
+            // 
+            this.pnlCar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(115)))), ((int)(((byte)(10)))));
+            this.pnlCar.Controls.Add(this.picCar);
+            this.pnlCar.Location = new System.Drawing.Point(206, 18);
+            this.pnlCar.Name = "pnlCar";
+            this.pnlCar.Size = new System.Drawing.Size(49, 47);
+            this.pnlCar.TabIndex = 0;
+            // 
+            // picCar
+            // 
+            this.picCar.Image = ((System.Drawing.Image)(resources.GetObject("picCar.Image")));
+            this.picCar.Location = new System.Drawing.Point(3, 3);
+            this.picCar.Name = "picCar";
+            this.picCar.Size = new System.Drawing.Size(43, 41);
+            this.picCar.TabIndex = 0;
+            this.picCar.TabStop = false;
+            // 
+            // loginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Controls.Add(this.pnlLogin);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "loginPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoCare Pro | Login Portal";
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.pnlRegister.ResumeLayout(false);
+            this.pnlRegister.PerformLayout();
+            this.pnlLoginForm.ResumeLayout(false);
+            this.pnlLoginForm.PerformLayout();
+            this.pnlCar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picCar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Panel pnlCar;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox picCar;
+        private Panel pnlRegister;
+        private Label lblUserName;
+        private TextBox txtRegUsername;
+        private Label lblRegEmail;
+        private TextBox txtRegEmail;
+        private TextBox txtOutputPass;
+        private Label lblRegPass;
+        private Panel pnlLoginForm;
+        private TextBox txtLoginPass;
+        private Label lblLoginPass;
+        private TextBox txtLoginEmail;
+        private Label lblLoginEmail;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoCare_Pro
+{
+    public partial class loginPage : Form
+    {
+        public loginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            this.btnLogin.BackColor= Color.FromArgb(232, 115, 10);
+            this.btnRegister.BackColor = Color.FromArgb(26, 31, 46);
+            this.pnlLoginForm.Visible = true;
+            this.pnlRegister.Visible = false;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            this.btnRegister.BackColor = Color.FromArgb(232, 115, 10);
+            this.btnLogin.BackColor = Color.FromArgb(26, 31, 46);
+            this.pnlRegister.Visible = true;
+            this.pnlLoginForm.Visible = false;
+        }
+    }
+}

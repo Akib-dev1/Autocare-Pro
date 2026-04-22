2c99b84dc24b295d68337f230153a077d0adf0d3
Rafi734 <abdurrahimrafi90@gmail.com>
2026-04-22 19:01:46 +0600

    1st push

--- Jobcard.cs
+++ Jobcard.cs
@@ -16,5 +16,10 @@ namespace AutoCare_Pro
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

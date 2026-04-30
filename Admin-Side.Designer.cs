093d7f87be6fe936db1e1f36cd5d83d07d8759f4
Rafi734 <abdurrahimrafi90@gmail.com>
2026-04-30 08:27:54 +0600

    REPORT PAGE

---
+++ Admin-Side.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace AutoCare_Pro
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminForm));
+            this.pnlAdminMenu = new System.Windows.Forms.Panel();
+            this.lblAdminLogo = new System.Windows.Forms.Label();
+            this.lblAdminDescription = new System.Windows.Forms.Label();
+            this.btnAdminNavlink1 = new System.Windows.Forms.Button();
+            this.btnAdminNavlink2 = new System.Windows.Forms.Button();
+            this.btnAdminNavlink3 = new System.Windows.Forms.Button();
+            this.btnAdminNavlink4 = new System.Windows.Forms.Button();
+            this.pnlAdminMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlAdminMenu
+            // 
+            this.pnlAdminMenu.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pnlAdminMenu.Controls.Add(this.btnAdminNavlink4);
+            this.pnlAdminMenu.Controls.Add(this.btnAdminNavlink3);
+            this.pnlAdminMenu.Controls.Add(this.btnAdminNavlink2);
+            this.pnlAdminMenu.Controls.Add(this.btnAdminNavlink1);
+            this.pnlAdminMenu.Controls.Add(this.lblAdminDescription);
+            this.pnlAdminMenu.Controls.Add(this.lblAdminLogo);
+            this.pnlAdminMenu.Location = new System.Drawing.Point(1, 1);
+            this.pnlAdminMenu.Name = "pnlAdminMenu";
+            this.pnlAdminMenu.Size = new System.Drawing.Size(269, 625);
+            this.pnlAdminMenu.TabIndex = 0;
+            // 
+            // lblAdminLogo
+            // 
+            this.lblAdminLogo.AutoSize = true;
+            this.lblAdminLogo.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdminLogo.ForeColor = System.Drawing.Color.White;
+            this.lblAdminLogo.Location = new System.Drawing.Point(12, 20);
+            this.lblAdminLogo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAdminLogo.Name = "lblAdminLogo";
+            this.lblAdminLogo.Size = new System.Drawing.Size(169, 24);
+            this.lblAdminLogo.TabIndex = 1;
+            this.lblAdminLogo.Text = "AUTOCARE PRO";
+            // 
+            // lblAdminDescription
+            // 
+            this.lblAdminDescription.AutoSize = true;
+            this.lblAdminDescription.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdminDescription.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblAdminDescription.Location = new System.Drawing.Point(12, 44);
+            this.lblAdminDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAdminDescription.Name = "lblAdminDescription";
+            this.lblAdminDescription.Size = new System.Drawing.Size(136, 20);
+            this.lblAdminDescription.TabIndex = 2;
+            this.lblAdminDescription.Text = "Service Ledger";
+            // 
+            // btnAdminNavlink1
+            // 
+            this.btnAdminNavlink1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnAdminNavlink1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(94)))), ((int)(((byte)(111)))));
+            this.btnAdminNavlink1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdminNavlink1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminNavlink1.ForeColor = System.Drawing.Color.White;
+            this.btnAdminNavlink1.Image = ((System.Drawing.Image)(resources.GetObject("btnAdminNavlink1.Image")));
+            this.btnAdminNavlink1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdminNavlink1.Location = new System.Drawing.Point(0, 99);
+            this.btnAdminNavlink1.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdminNavlink1.Name = "btnAdminNavlink1";
+            this.btnAdminNavlink1.Size = new System.Drawing.Size(269, 59);
+            this.btnAdminNavlink1.TabIndex = 3;
+            this.btnAdminNavlink1.Text = "Job Cards";
+            this.btnAdminNavlink1.UseVisualStyleBackColor = false;
+            // 
+            // btnAdminNavlink2
+            // 
+            this.btnAdminNavlink2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnAdminNavlink2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(94)))), ((int)(((byte)(111)))));
+            this.btnAdminNavlink2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdminNavlink2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminNavlink2.ForeColor = System.Drawing.Color.White;
+            this.btnAdminNavlink2.Image = ((System.Drawing.Image)(resources.GetObject("btnAdminNavlink2.Image")));
+            this.btnAdminNavlink2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdminNavlink2.Location = new System.Drawing.Point(0, 175);
+            this.btnAdminNavlink2.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdminNavlink2.Name = "btnAdminNavlink2";
+            this.btnAdminNavlink2.Size = new System.Drawing.Size(272, 59);
+            this.btnAdminNavlink2.TabIndex = 4;
+            this.btnAdminNavlink2.Text = "Customers";
+            this.btnAdminNavlink2.UseVisualStyleBackColor = false;
+            // 
+            // btnAdminNavlink3
+            // 
+            this.btnAdminNavlink3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnAdminNavlink3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(94)))), ((int)(((byte)(111)))));
+            this.btnAdminNavlink3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdminNavlink3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminNavlink3.ForeColor = System.Drawing.Color.White;
+            this.btnAdminNavlink3.Image = ((System.Drawing.Image)(resources.GetObject("btnAdminNavlink3.Image")));
+            this.btnAdminNavlink3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdminNavlink3.Location = new System.Drawing.Point(0, 251);
+            this.btnAdminNavlink3.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdminNavlink3.Name = "btnAdminNavlink3";
+            this.btnAdminNavlink3.Size = new System.Drawing.Size(272, 59);
+            this.btnAdminNavlink3.TabIndex = 5;
+            this.btnAdminNavlink3.Text = "Inventory";
+            this.btnAdminNavlink3.UseVisualStyleBackColor = false;
+            // 
+            // btnAdminNavlink4
+            // 
+            this.btnAdminNavlink4.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnAdminNavlink4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(94)))), ((int)(((byte)(111)))));
+            this.btnAdminNavlink4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdminNavlink4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdminNavlink4.ForeColor = System.Drawing.Color.White;
+            this.btnAdminNavlink4.Image = ((System.Drawing.Image)(resources.GetObject("btnAdminNavlink4.Image")));
+            this.btnAdminNavlink4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdminNavlink4.Location = new System.Drawing.Point(0, 328);
+            this.btnAdminNavlink4.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdminNavlink4.Name = "btnAdminNavlink4";
+            this.btnAdminNavlink4.Size = new System.Drawing.Size(270, 59);
+            this.btnAdminNavlink4.TabIndex = 6;
+            this.btnAdminNavlink4.Text = "Reports";
+            this.btnAdminNavlink4.UseVisualStyleBackColor = false;
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(1137, 626);
+            this.Controls.Add(this.pnlAdminMenu);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "AdminForm";
+            this.Text = "Admin_Side";
+            this.pnlAdminMenu.ResumeLayout(false);
+            this.pnlAdminMenu.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlAdminMenu;
+        private System.Windows.Forms.Label lblAdminLogo;
+        private System.Windows.Forms.Label lblAdminDescription;
+        private System.Windows.Forms.Button btnAdminNavlink1;
+        private System.Windows.Forms.Button btnAdminNavlink2;
+        private System.Windows.Forms.Button btnAdminNavlink3;
+        private System.Windows.Forms.Button btnAdminNavlink4;
+    }
+}
---
+++ Reports.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace AutoCare_Pro
+{
+    partial class Reports
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reports));
+            this.pnlReportTopBar = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblReportTitle = new System.Windows.Forms.Label();
+            this.lblReportName = new System.Windows.Forms.Label();
+            this.txtReportSearch = new System.Windows.Forms.TextBox();
+            this.btnReportSearchButton = new System.Windows.Forms.Button();
+            this.lblReportSalesAnalytics = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlReportTopBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlReportTopBar
+            // 
+            this.pnlReportTopBar.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.pnlReportTopBar.Controls.Add(this.btnReportSearchButton);
+            this.pnlReportTopBar.Controls.Add(this.txtReportSearch);
+            this.pnlReportTopBar.Controls.Add(this.lblReportName);
+            this.pnlReportTopBar.Controls.Add(this.lblReportTitle);
+            this.pnlReportTopBar.Controls.Add(this.pictureBox1);
+            this.pnlReportTopBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlReportTopBar.Name = "pnlReportTopBar";
+            this.pnlReportTopBar.Size = new System.Drawing.Size(1203, 70);
+            this.pnlReportTopBar.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1136, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblReportTitle
+            // 
+            this.lblReportTitle.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReportTitle.Location = new System.Drawing.Point(16, 15);
+            this.lblReportTitle.Name = "lblReportTitle";
+            this.lblReportTitle.Size = new System.Drawing.Size(201, 35);
+            this.lblReportTitle.TabIndex = 1;
+            this.lblReportTitle.Text = "The Machinist Ledger";
+            this.lblReportTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblReportName
+            // 
+            this.lblReportName.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReportName.ForeColor = System.Drawing.Color.Brown;
+            this.lblReportName.Location = new System.Drawing.Point(1042, 20);
+            this.lblReportName.Name = "lblReportName";
+            this.lblReportName.Size = new System.Drawing.Size(88, 25);
+            this.lblReportName.TabIndex = 2;
+            this.lblReportName.Text = "ADMIN";
+            this.lblReportName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtReportSearch
+            // 
+            this.txtReportSearch.Location = new System.Drawing.Point(242, 20);
+            this.txtReportSearch.Multiline = true;
+            this.txtReportSearch.Name = "txtReportSearch";
+            this.txtReportSearch.Size = new System.Drawing.Size(294, 30);
+            this.txtReportSearch.TabIndex = 3;
+            // 
+            // btnReportSearchButton
+            // 
+            this.btnReportSearchButton.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportSearchButton.Image = ((System.Drawing.Image)(resources.GetObject("btnReportSearchButton.Image")));
+            this.btnReportSearchButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnReportSearchButton.Location = new System.Drawing.Point(534, 20);
+            this.btnReportSearchButton.Name = "btnReportSearchButton";
+            this.btnReportSearchButton.Size = new System.Drawing.Size(130, 30);
+            this.btnReportSearchButton.TabIndex = 4;
+            this.btnReportSearchButton.Text = "Search";
+            this.btnReportSearchButton.UseVisualStyleBackColor = true;
+            // 
+            // lblReportSalesAnalytics
+            // 
+            this.lblReportSalesAnalytics.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReportSalesAnalytics.Location = new System.Drawing.Point(16, 107);
+            this.lblReportSalesAnalytics.Name = "lblReportSalesAnalytics";
+            this.lblReportSalesAnalytics.Size = new System.Drawing.Size(177, 35);
+            this.lblReportSalesAnalytics.TabIndex = 1;
+            this.lblReportSalesAnalytics.Text = "Sales Analytics";
+            this.lblReportSalesAnalytics.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(20, 185);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(308, 111);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(429, 185);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(308, 111);
+            this.flowLayoutPanel2.TabIndex = 3;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(822, 185);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(308, 111);
+            this.flowLayoutPanel3.TabIndex = 3;
+            // 
+            // Reports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.lblReportSalesAnalytics);
+            this.Controls.Add(this.pnlReportTopBar);
+            this.Name = "Reports";
+            this.Size = new System.Drawing.Size(1204, 649);
+            this.pnlReportTopBar.ResumeLayout(false);
+            this.pnlReportTopBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlReportTopBar;
+        private System.Windows.Forms.Label lblReportTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblReportName;
+        private System.Windows.Forms.TextBox txtReportSearch;
+        private System.Windows.Forms.Button btnReportSearchButton;
+        private System.Windows.Forms.Label lblReportSalesAnalytics;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+    }
+}
